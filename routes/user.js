4e68aa581461fb0027cd8c0a955abc795ed037ab
jibinemb <jibinembrayil98@gmail.com>
2022-12-11 18:28:31 +0530

    verify login is not there

---
+++ routes/user.js
@@ -0,0 +1,63 @@
+var express = require('express');
+const { response } = require('../app');
+var router = express.Router();
+const productHelpers = require('../helpers/product-helpers');
+const userHelpers = require('../helpers/user-helpers')
+
+/* GET home page. */
+router.get('/',async function(req, res, next) {
+  let user=req.session.user
+  console.log(user);
+  productHelpers.getAllProducts().then((products)=>{
+   
+    res.render('user/view-products',{  products,user })
+  })
+});
+router.get('/login',(req,res)=>{
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else{
+    
+    res.render('user/login',{"loginErr":req.session.loginErr})
+    req.session.loginErr=false
+  }  
+})
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+})
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response)
+  })
+
+})
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true
+      req.session.user=response.user
+      res.redirect('/')
+
+    }else{
+      req.session.loginErr=true
+      res.redirect('/login')
+    }
+  })
+
+})
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
+})
+router.get('/cart',(req,res)=>{
+  res.render('user/cart')
+})
+router
+router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
+  console.log("api call",req.session.user._id);
+  userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
+    res.redirect('/')
+    // res.json({status:true})
+  })
+})
+module.exports = router;
